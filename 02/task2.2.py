b2c78148b9bd15afe7b5752e5f48ebcfd5830b26
riboux mathilde <mathilde.riboux@epitech.eu>
2026-09-08 12:01:40 +0200

    m

--- 02/task2.2.py
+++ 02/task2.2.py
@@ -1,1 +1,7 @@
 print(17**1024)
+
+from math import lcm
+
+print(lcm(*range(1, 21)))   # fonctionne maintenant sans le préfixe !
+print(lcm(*range(1, 201)))   # fonctionne maintenant sans le préfixe !
+print(lcm(*range(1, 2001)))   # fonctionne maintenant sans le préfixe !
